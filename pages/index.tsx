9685734c0b165bf99bef8a676ee97e98a53b8f5e
HananMustafaEL-hady <hananmustafa.hassan@gmail.com>
2021-11-09 15:15:25 +0200

    home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,46 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <div className="row ">
-    Home
-    </div>
+    <div className="bg">
+    <div className=" col-lg-12 col-md-10 col-sm-10 ">
+    <div className="card border-0 rounded-lg  mx-auto container__Image  " >
+    <style jsx>{`
+    .container__Image {
+        width: 43rem;
+        background-color: #37474f;
 
-  )
+    }
+  
+  `}</style>
+         <Image
+  src="/movie.png"
+  alt="Picture of the error"
+  width={500}
+  height={500}
+/>
+        <div className="card-body mx-auto ">
+            <Link href="/usemoive">
+            <a type="button " 
+            className="btn btn-sm btn-warning text-white d-flex justify-content-between"> <p className="pt-3">watch Movies</p>            <Image
+            src="/Popcorns-pana.png"
+            alt="Picture of the error"
+            width={50}
+            height={50}
+          /></a>
+  
+            </Link>
+ 
+        </div>
+    </div>
+</div>
+</div>
+)
+
+  
 }
 
 export default Home
